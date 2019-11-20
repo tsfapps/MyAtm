b42e7ae0860808bd46f118a39919cf33dd9733f0
tsfapps <appslelo.com@gmail.com>
2019-11-20 13:23:32 +0530

    notes of 100 included with small amount too

--- app/src/main/java/com/appslelo/myatm/MainActivity.kt
+++ app/src/main/java/com/appslelo/myatm/MainActivity.kt
@@ -2,14 +2,12 @@ package com.appslelo.myatm
 
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.appslelo.myatm.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import java.util.function.BiFunction
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,35 +32,71 @@ class MainActivity : AppCompatActivity() {
             }else if (number>15000)
             {
                 Toast.makeText(this,"ATM Cash Limit exceeds.",Toast.LENGTH_LONG).show()
-            }else if(number%100>0){
+            }else if(number%100!=0){
                 Toast.makeText(this,"Please insert valid amount",Toast.LENGTH_LONG).show()
             }
             else {
-                val denominations = intArrayOf(2000, 500, 200, 100)
-                val amount: Int = number
-                val count: IntArray = breakdown(denominations, amount)
 
-                for (i in denominations.indices) {
-                    if (count[i] > 0) {
-                        tvMoney.append(denominations[i].toString()+ " : " + count[i].toString()+"\n")
+                val notes = intArrayOf(100, 200, 500, 2000)
+                val amount: Int = number
+
+                val hashMap = HashMap<String, Int>()
+                if (hasLowestDenomination(notes, 0, notes.size, amount, hashMap)) {
+                    for (i in 0..2) {
+                        val n = hashMap[notes[i].toString()]!!
+                        if (n * notes[i] >= notes[i + 1]) {
+                            hashMap.replace(
+                                notes[i].toString(),
+                                n * notes[i] / notes[i + 1]
+                            )
+                            hashMap.replace(
+                                notes[i + 1].toString(),
+                                hashMap[notes[i + 1].toString()]!! + n * notes[i] / notes[i + 1]
+                            )
+                        }
+                    }
+                    for ((key, value) in hashMap) {
+                        if (value == 1 || value == 0) {
+                            tvMoney.append("$value note of Rs.$key\n")
+                        } else {
+                            tvMoney.append("$value note of Rs.$key\n")
+                            println("$value notes of Rs.$key")
+                        }
                     }
                 }
-
 
             }
         }
     }
 
 
-private fun breakdown(denominations: IntArray, amount: Int): IntArray {
-    var amount = amount
-    val count = IntArray(denominations.size)
 
-    for (i in denominations.indices) {  while (amount >= denominations[i]) {
-            count[i]++
-            amount -= denominations[i]
-        }
+    private fun hasLowestDenomination( notes: IntArray , i: Int,  length: Int, sum: Int,  hashMap: HashMap<String, Int>): Boolean {
+        if (i == length) return false
+        if (sum < 0) return false
+        if (i == length - 1 && notes[i] != sum) return false
+        hashMap.compute(
+            notes[i].toString(),
+            BiFunction<String, Int?, Int?> { key: String?, `val`: Int? -> if (`val` == null) 1 else `val` + 1 }
+        )
+        if (notes[i] == sum) return true
+        if (hasLowestDenomination(
+                notes,
+                i + 1,
+                length,
+                sum - notes[i],
+                hashMap
+            ) || hasLowestDenomination(notes, i, length, sum - notes[i], hashMap)
+        ) return true
+        hashMap.compute(
+            notes[i].toString(),
+            BiFunction<String, Int?, Int?> { key: String?, `val`: Int? -> if (`val` == null) 0 else `val` - 1 }
+        )
+        return false
     }
-    return count
-}
+
+
+
+
+
 }
